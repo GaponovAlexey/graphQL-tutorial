c36aae73076af83078f39a6477755b770a57ed0d
Kekelberi <alexei.gaponov@gmail.com>
2022-01-24 19:21:12 +0300

    save

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require("../shema/shema")
 
 const app = express();
-const PORT = 3005;
+const PORT = 3000;
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
 
 
 app.listen(PORT, err => {
--- shema/shema.js
+++ shema/shema.js
@@ -1,12 +1,70 @@
 const qraphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString } = qraphql
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = qraphql
+
+const movies = [
+  { id: '1', name: 'Alexey', genre: 'screamer', directorId: '1' },
+  { id: '2', name: 'my life', genre: 'comedi', directorId: '2' },
+  { id: '3', name: 'eards', genre: 'distruct', directorId: '3' },
+  { id: '4', name: 'dom', genre: 'triler', directorId: '4' },
+]
+const directors = [
+  { id: '1', name: 'Quntin tarantino', age: 70 },
+  { id: '2', name: 'PUTIN', age: 1000 },
+  { id: '3', name: 'Elizaveta', age: 40 },
+  { id: '4', name: 'Malkolm', age: 14 },
+]
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find((director) => director.id === parent.directorId)
+      },
+    },
   }),
+})
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+})
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return movies.find((movie) => movie.id == args.id)
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.find((director) => director.id == args.id)
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: Query,
 })
